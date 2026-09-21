bc5e414bb2cc2cffeba72b79983ecf41d38f8da7
lionelgossec <lionel.gossec@gmail.com>
2026-09-21 23:18:36 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='charcut-tracker-v6-4';
+const CACHE='charcut-tracker-v6-4-2';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
